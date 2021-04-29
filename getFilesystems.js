0f73a5b744426d6556fdb608d34323953fb426b3
prabu <prabuarjunan@gmail.com>
2021-04-28 17:34:12 -0700

    removed bearer token
    removed bearer token

--- getFilesystems.js
+++ getFilesystems.js
@@ -6,7 +6,7 @@ function getFilesystems(){
     'url': 'https://cloudvolumesgcp-api.netapp.com/v2/projects/779740114201/locations/us-central1/Volumes',
     'headers': {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJSUzI1NiIsICJraWQiOiAiZWNjZWU4NzExNTU3MDYzZTQ3OTUxYWI2YWMzMDE4ZGYzNmYxMGFjZiJ9.eyJpc3MiOiAicHJhYnUtdGVzdDFAbmN2LWJldGEtZGVtby5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJzdWIiOiAicHJhYnUtdGVzdDFAbmN2LWJldGEtZGVtby5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJpYXQiOiAxNjE5NjU0NDc5LCAiZXhwIjogMTYxOTY1ODA3OSwgImF1ZCI6ICJodHRwczovL2Nsb3Vkdm9sdW1lc2djcC1hcGkubmV0YXBwLmNvbSJ9.FszzRFwlCIdE70jCzzXQbAg1emF3sgnSKRJXGxIdGjw6XB0Y78OUI9z7e_TnbvavLwePEsQe5rzcfMP7FbtZK4C4b29g_QZmXd0LniPq1-2a9EWLu-DgFDUMQXFUmPU7JftWa0A4DL86JoIkbPV8ZaSx1o0Ngh5alaVfk7vtO-i5jp5V59iDT4SIoBYXzpNX0Q06wyjfvquW0hr_vFNxDeOWeSt2QcfC9pbJeGo7LNj3y3ePNzBUQD0I5elMklsTrHpRyaey6Q72TgNoSQx92nOSovOhqZoaez4vLIIhJ5Y6TVo2xwg0F_a_U2TD6A__GoKxV9re-LiU21x2T1MScg'
+      'Authorization': 'Bearer Enter your bearer token here'
     }
   };
   request(options, function (error, response) {
